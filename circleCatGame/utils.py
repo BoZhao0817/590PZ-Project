d99ca8fbeb964087ae0c41186dc233e306aa2435
widen <yuangao719@gmail.com>
2019-11-15 13:04:58 -0600

    little modif

--- circleCatGame/utils.py
+++ circleCatGame/utils.py
@@ -6,5 +6,5 @@ def generate_random_locations(n, loc_dict):
     while True:
         i = randint(0,n-1)
         j = randint(0,n-1)
-        if (i,j) not in loc_dict:
-            return (i,j)
+        if (i, j) not in loc_dict:
+            return (i, j)
